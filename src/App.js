1df84f7b061055eae2d960adc7ee7a4fc9d743bc
GLADI8R <danishmehmuda@gmail.com>
2021-08-12 19:15:57 +0530

    Add Global State

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { AddTransaction } from './components/AddTransaction';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
 import './styles/styles.scss';
 
 function App() {
   return (
+    <GlobalProvider>
     <div className="center header">
       <h1 className="sans">Expense Tracker</h1>
       <div className='container'>
@@ -15,6 +17,7 @@ function App() {
         <AddTransaction />
       </div>
     </div>
+    </GlobalProvider> 
   );
 }
 
---
+++ src/context/GlobalState.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+
+//Initial State
+const initialState = {
+	transactions: [
+		{ id: 1, text: 'Flower', amount: -20 },
+  		{ id: 2, text: 'Salary', amount: 300 },
+  		{ id: 3, text: 'Book', amount: -10 },
+  		{ id: 4, text: 'Camera', amount: 150 }
+	]
+}
+
+// Create Context
+export const GlobalContext = createContext(initialState);
+
+
+// For all files to have access to GlobalContext we need Provider
+//Provider Component
+//it takes props children and use destructuring
+export const GlobalProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	return (
+		<GlobalContext.Provider value={{transactions: state.transactions}}>
+			{children}
+		</GlobalContext.Provider>
+	);
+}
