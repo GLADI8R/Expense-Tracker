4f529037ef85f578ca1fc35693dd4a5aabfb4b1b
GLADI8R <danishmehmuda@gmail.com>
2021-08-13 20:18:36 +0530

    Add Functionality to get total income and expenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,25 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 import '../styles/styles.scss';
 
 export const IncomeExpenses = () => {
+	//use destructuring to get transactions
+	const { transactions } = useContext(GlobalContext);
+
+	const amounts = transactions.map(transaction => transaction.amount);
+	
+	const income = amounts.filter(item => item > 0).reduce((amount, item) => (amount += item), 0).toFixed(2);							
+	const expense = (amounts.filter(item => item < 0).reduce((amount, item) => (amount += item), 0) * -1).toFixed(2);							
+
 	return (
 		<div className="inc-exp-cont">
 			<div>
 				<h4>Income</h4>
-				<p className='green'>$0.00</p>
+				<p className='green'>${income}</p>
 			</div>
 			<div>
 				<h4>Expenses</h4>
-				<p className='red'>$0.00</p>
+				<p className='red'>${expense}</p>
 			</div>
 		</div>
 	)
