18e813256228e16ae8e9e5cb6148d64d8d06c26b
GLADI8R <danishmehmuda@gmail.com>
2021-08-13 20:11:24 +0530

    Add functionality to get total balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 import '../styles/styles.scss';
 
 export const Balance = () => {
+	//use destructuring to get transactions
+	const { transactions } = useContext(GlobalContext);
+
+	const amounts = transactions.map(transaction => transaction.amount);
+	const total = amounts.reduce((amount, item) => (amount += item), 0).toFixed(2);
+
 	return (
 		<>
 			<h4>Your Balance:</h4>
-			<h2>$0.00</h2>
+			<h2>${total}</h2>
 		</>
 	)
 }
