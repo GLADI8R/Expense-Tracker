a3d5b62d3dd73363845e84d2e4ab2afb2e3c2d25
GLADI8R <danishmehmuda@gmail.com>
2021-08-14 13:11:03 +0530

    Add delete transaction button

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,6 +4,9 @@ export const Transaction = (props) => {
 	const sign = props.transaction.amount < 0 ? '-' : '+';
 
 	return (
-		<li className={props.transaction.amount < 0 ? 'red' : 'green'}>{props.transaction.text}<span>{sign}${Math.abs(props.transaction.amount)}</span></li>
+		<li className={props.transaction.amount < 0 ? 'red' : 'green'}>
+			{props.transaction.text}<span>{sign}${Math.abs(props.transaction.amount)}</span>
+			<button /*onClick={() => deleteTransaction(props.transaction.id)}*/ className="delete-btn">x</button>	
+		</li>
 	)
 }
